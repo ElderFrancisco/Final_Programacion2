41eb88f4e5c815e77fb1946ab29ba68b06a9d0eb
ElderFrancisco <franciscoelder0@gmail.com>
2024-12-11 11:56:42 -0300

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonMax = new System.Windows.Forms.Button();
+            this.buttonMin = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar saldos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(117, 17);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(254, 199);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonMin);
+            this.groupBox1.Controls.Add(this.buttonMax);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(398, 226);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculador";
+            // 
+            // buttonMax
+            // 
+            this.buttonMax.Location = new System.Drawing.Point(14, 68);
+            this.buttonMax.Name = "buttonMax";
+            this.buttonMax.Size = new System.Drawing.Size(97, 43);
+            this.buttonMax.TabIndex = 3;
+            this.buttonMax.Text = "Maximo";
+            this.buttonMax.UseVisualStyleBackColor = true;
+            this.buttonMax.Click += new System.EventHandler(this.buttonMax_Click);
+            // 
+            // buttonMin
+            // 
+            this.buttonMin.Location = new System.Drawing.Point(14, 117);
+            this.buttonMin.Name = "buttonMin";
+            this.buttonMin.Size = new System.Drawing.Size(97, 43);
+            this.buttonMin.TabIndex = 4;
+            this.buttonMin.Text = "Minimo";
+            this.buttonMin.UseVisualStyleBackColor = true;
+            this.buttonMin.Click += new System.EventHandler(this.buttonMin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 250);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonMin;
+        private System.Windows.Forms.Button buttonMax;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Variables globales para el máximo y mínimo saldo
+        decimal maxSaldo = decimal.MinValue;
+        decimal minSaldo = decimal.MaxValue;
+        string maxId = null;
+        string minId = null;
+        string maxName = null;
+        string minName = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Ruta al archivo TXT
+            string filePath = Path.Combine(Application.StartupPath, "data.txt");
+
+            // Verificar que el archivo exista
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("El archivo no existe. Por favor, verifica la ruta.");
+                return;
+            }
+
+            // Limpiar el ListBox
+            listBox1.Items.Clear();
+
+            // Variables de corte de control
+            string idActual = null; // ID actual que estamos procesando
+            decimal saldoAcumulado = 0; // Saldo acumulado para el ID actual
+
+            // Leer archivo línea por línea
+            foreach (string linea in File.ReadAllLines(filePath))
+            {
+                // Dividir los campos de la línea
+                string[] campos = linea.Split(';');
+                if (campos.Length != 4) continue;
+
+                string id = campos[0];
+                string nombre = campos[1];
+                string operacion = campos[2].ToLower();
+                decimal monto;
+
+                // Validar que el monto sea numérico
+                if (!decimal.TryParse(campos[3], out monto)) continue;
+
+                // Corte de control: si el ID cambia, mostramos el saldo y reiniciamos acumulador
+                if (idActual != null && id != idActual)
+                {
+                    listBox1.Items.Add($"ID: {idActual}, {nombre}, Saldo: {saldoAcumulado}");
+                    
+                    // Actualizar el máximo y mínimo
+                    if (saldoAcumulado > maxSaldo)
+                    {
+                        maxSaldo = saldoAcumulado;
+                        maxName = nombre;
+                        maxId = idActual;
+                    }
+                    if (saldoAcumulado < minSaldo)
+                    {
+                        minSaldo = saldoAcumulado;
+                        minName = nombre;
+                        minId = idActual;
+                    }
+
+                    saldoAcumulado = 0; // Reiniciamos acumulador
+                }
+
+                // Actualizamos ID actual
+                idActual = id;
+
+                // Sumar o restar el monto al saldo acumulado
+                if (operacion == "suma")
+                    saldoAcumulado += monto;
+                else if (operacion == "resta")
+                    saldoAcumulado -= monto;
+            }
+
+            // Mostrar el último saldo acumulado (para el último ID del archivo)
+            if (idActual != null)
+            {
+                listBox1.Items.Add($"ID: {idActual}, Saldo: {saldoAcumulado}");
+
+                // Actualizar el máximo y mínimo
+                if (saldoAcumulado > maxSaldo)
+                {
+                    maxSaldo = saldoAcumulado;
+                    maxId = idActual;
+                }
+                if (saldoAcumulado < minSaldo)
+                {
+                    minSaldo = saldoAcumulado;
+                    minId = idActual;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonMax_Click(object sender, EventArgs e)
+        {
+            if (maxId != null)
+            {
+                MessageBox.Show($"El usuario con el máximo saldo es ID: {maxId}, Nombre: {maxName}, Saldo: {maxSaldo}");
+            }
+            else
+            {
+                MessageBox.Show("No se ha procesado ningún saldo.");
+            }
+        }
+
+        private void buttonMin_Click(object sender, EventArgs e)
+        {
+            if (minId != null)
+            {
+                MessageBox.Show($"El usuario con el mínimo saldo es ID: {minId}, Nombre: {minName}, Saldo: {minSaldo}");
+            }
+            else
+            {
+                MessageBox.Show("No se ha procesado ningún saldo.");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
